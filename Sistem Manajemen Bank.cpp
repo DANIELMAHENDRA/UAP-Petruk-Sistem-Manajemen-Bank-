697061c43389262452753a8bb37abcc979315be0
sulthon aris setiawan <153345768+sulthonarissetiawan@users.noreply.github.com>
2024-06-11 19:58:14 +0700

    Update Sistem Manajemen Bank.cpp

--- Sistem Manajemen Bank.cpp
+++ Sistem Manajemen Bank.cpp
@@ -17,3 +17,32 @@ struct Transaksi {
         this->waktuTransaksi = time(nullptr);
     }
 };
+
+class Akun {
+private:
+    int nomorAkun;
+    string nama;
+    double saldo;
+    string namaPengguna;
+    string kataSandi;
+    vector<Transaksi> riwayatTransaksi;
+public:
+    Akun() {
+        nomorAkun = 0;
+        nama = "";
+        saldo = 0.0;
+        namaPengguna = "";
+        kataSandi = "";
+    }
+    Akun(int nomorAkun, string nama, double saldo, string namaPengguna, string kataSandi) {
+        this->nomorAkun = nomorAkun;
+        this->nama = nama;
+        this->saldo = saldo;
+        this->namaPengguna = namaPengguna;
+        this->kataSandi = kataSandi;
+    }
+    void deposit(double jumlah) {
+        saldo += jumlah;
+        cout << "Berhasil menambahkan " << jumlah << " ke akun " << nomorAkun << endl;
+        catatTransaksi("Setor", jumlah);
+    }
