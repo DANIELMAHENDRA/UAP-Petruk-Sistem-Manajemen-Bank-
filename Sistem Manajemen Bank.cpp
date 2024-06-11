be467aaf33951df68fbff03460ed04a953d4d04f
DANIELMAHENDRA <151292335+DANIELMAHENDRA@users.noreply.github.com>
2024-06-11 19:56:17 +0700

    Update Sistem Manajemen Bank.cpp

--- Sistem Manajemen Bank.cpp
+++ Sistem Manajemen Bank.cpp
@@ -3,3 +3,17 @@
 #include <iomanip>
 #include <ctime>
 using namespace std;
+
+struct Transaksi {
+    string jenisTransaksi;
+    int nomorAkun;
+    double jumlah;
+    time_t waktuTransaksi;
+
+    Transaksi(string jenisTransaksi, int nomorAkun, double jumlah) {
+        this->jenisTransaksi = jenisTransaksi;
+        this->nomorAkun = nomorAkun;
+        this->jumlah = jumlah;
+        this->waktuTransaksi = time(nullptr);
+    }
+};
